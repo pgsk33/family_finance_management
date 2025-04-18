4a56cf0de8be62ef24d86fc07cfb7266eeb81fbb
p_gsk <PaulGesekus@proton.me>
2025-04-18 20:30:32 +0200

    Update family_finance_management.java

--- family_finance_management.java
+++ family_finance_management.java
@@ -1,9 +1,4 @@
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class FamilyFinanceManagement {
+{
     Connection connection;
 
     {
@@ -24,25 +19,25 @@ public class FamilyFinanceManagement {
     private void mainLoop() {
         try {
             while (true) {
-                System.out.println("Was möchtest du tun?");
-                System.out.println("(0) Programm beenden (1) Daten Ausgeben (2) Nutzer hinzufügen (3) Nutzer Ausgeben (4) Datenbanken Zurücksetzen (5) Eintrag hinzufügen");
+                System.out.println("Choose a function");
+                System.out.println("(0) exit program (1) output balance (2) add user (3) output user (4) reset database (5) add entry");
                 switch (sc.nextInt()) {
                     case 0:
                         System.exit(0);
                     case 1:
-                        einträgeAusgeben();
+                        outputBalance();
                         break;
                     case 2:
-                        nutzerhinzufuegen();
+                        addUser();
                         break;
                     case 3:
-                        nutzerausgeben();
+                        outputAllUser();
                         break;
                     case 4:
                         resetDatabase();
                         break;
                     case 5:
-                        eintragHinzufügen();
+                        addEntry();
                 }
             }
         } catch (SQLException e) {
@@ -50,18 +45,18 @@ public class FamilyFinanceManagement {
         }
     }
 
-    public void einträgeAusgeben() throws SQLException {
-        double stand = 0;
+    public void outputBalance() throws SQLException {
+        double balance = 0;
         System.out.println("Zwischen welchen 2 Nutzern soll der Schuldenstand ermittelt werden?");
-        int nid1 = getNid("1");
-        int nid2 = getNid("1");
+        int userID1 = askForAndReturnUserID();
+        int userID2 = askForAndReturnUserID();
 
-        Statement allesAusgebenStatement = connection.createStatement();
-        ResultSet resultSet = allesAusgebenStatement.executeQuery("SELECT e.IDeintrag, e.datum, e.betrag, e.stand, e.kommentar, n1.nutzername AS vorstrecker_name, n2.nutzername AS bezahler_name, e.vorstreckerID, e.bezahlerID " +
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT e.IDeintrag, e.datum, e.betrag, e.stand, e.kommentar, n1.nutzername AS vorstrecker_name, n2.nutzername AS bezahler_name, e.vorstreckerID, e.bezahlerID " +
                 "FROM einträge e " +
                 "LEFT JOIN nutzer n1 ON e.vorstreckerID = n1.IDnutzer " +
                 "LEFT JOIN nutzer n2 ON e.bezahlerID = n2.IDnutzer " +
-                "WHERE (e.vorstreckerID = " + nid1 + " AND e.bezahlerID = " + nid2 + ") OR (e.vorstreckerID = " + nid2 + " AND e.bezahlerID = " + nid1 + ") OR e.vorstreckerID = " + nid1 + " OR e.bezahlerID = " + nid1 + " OR e.vorstreckerID = " + nid2 + " OR e.bezahlerID = " + nid2 + " " +
+                "WHERE (e.vorstreckerID = " + userID1 + " AND e.bezahlerID = " + userID2 + ") OR (e.vorstreckerID = " + userID2 + " AND e.bezahlerID = " + userID1 + ") OR e.vorstreckerID = " + userID1 + " OR e.bezahlerID = " + userID1 + " OR e.vorstreckerID = " + userID2 + " OR e.bezahlerID = " + userID2 + " " +
                 "ORDER BY e.datum;");
         while (resultSet.next()) {
             System.out.print(resultSet.getDate("datum"));
@@ -69,47 +64,44 @@ public class FamilyFinanceManagement {
             System.out.print(resultSet.getInt("betrag"));
             System.out.print("  ");
 
-            if (nid1 == resultSet.getInt("vorstreckerID")){
-                stand += resultSet.getInt("betrag");
+            if (userID1 == resultSet.getInt("vorstreckerID")) {
+                balance += resultSet.getInt("betrag");
             } else {
-                stand -= resultSet.getInt("betrag");
+                balance -= resultSet.getInt("betrag");
             }
 
-            System.out.print(stand + "   ");
+            System.out.print(balance + "   ");
             System.out.print(resultSet.getString("kommentar"));
             System.out.print("  ");
-            System.out.print(resultSet.getInt("vorstreckerID"));
+            System.out.print(getUsernameWithID(resultSet.getInt("vorstreckerID"), connection));
             System.out.print("  ");
-            System.out.print(resultSet.getInt("bezahlerID"));
+            System.out.print(getUsernameWithID(resultSet.getInt("bezahlerID"),connection));
             System.out.println("  ");
         }
-        allesAusgebenStatement.close();
+        statement.close();
         resultSet.close();
-        Statement myps = connection.createStatement();
-        int searchID = nid1;
-        String nid1nn = "error";
-        String nid2nn = "error";
-        ResultSet myrs = myps.executeQuery("Select nutzername from nutzer where IDnutzer = " + searchID + ";");
-        while (myrs.next()){
-            nid1nn = myrs.getString("nutzername");
-        }
-        searchID = nid2;
-        ResultSet myrs2 = myps.executeQuery("Select nutzername from nutzer where IDnutzer = " + searchID + ";");
-        while (myrs2.next()){
-            nid2nn = myrs2.getString("nutzername");
-        }
-        System.out.println(nid1nn + " bekommt von " + nid2nn + " " + stand + " Euro.");
+
+        System.out.println(getUsernameWithID(userID1, connection) +" bekommt von " + getUsernameWithID(userID2, connection) +" " + balance + " Euro.");
     }
 
-    private int getNid(String userID) throws SQLException {
-        nutzerausgeben();
-        System.out.print("Nutzer id " + userID);
+    private static String getUsernameWithID(int searchID, Connection connection) throws SQLException {
+        Statement myps = connection.createStatement();
+        ResultSet myrs = myps.executeQuery("Select nutzername from nutzer where IDnutzer = " + searchID + ";");
+        while (myrs.next()) {
+            return myrs.getString("nutzername");
+        }
+        return "error";
+    }
+
+    private int askForAndReturnUserID() throws SQLException {
+        outputAllUser();
+        System.out.println("Geben sie userID an: ");
         return sc.nextInt();
     }
 
-    public void nutzerausgeben() throws SQLException {
-        Statement allesAusgebenStatement = connection.createStatement();
-        ResultSet resultSet = allesAusgebenStatement.executeQuery("Select IDnutzer, nutzername from nutzer");
+    public void outputAllUser() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select IDnutzer, nutzername from nutzer");
         while (resultSet.next()) {
             System.out.print(resultSet.getInt("IDnutzer"));
             System.out.print("  ");
@@ -117,31 +109,31 @@ public class FamilyFinanceManagement {
             System.out.println("");
         }
         resultSet.close();
-        allesAusgebenStatement.close();
+        statement.close();
     }
 
-    public void nutzerhinzufuegen() throws SQLException {
+    public void addUser() throws SQLException {
         System.out.print("Geben sie einen Nutzernamen an: ");
-        String nutzername = sc.next();
+        String username = sc.next();
         System.out.print("Geben sie ein Passwort an: ");
-        String passwort = sc.next();
-        PreparedStatement stnutzerHinzufuegen = connection.prepareStatement("insert into nutzer values\n" +
-                "(null, '" + nutzername + "', '" + passwort + "');");
-        stnutzerHinzufuegen.execute();
-        stnutzerHinzufuegen.close();
+        String password = sc.next();
+        PreparedStatement addUserStatement = connection.prepareStatement("insert into nutzer values\n" +
+                "(null, '" + username + "', '" + password + "');");
+        addUserStatement.execute();
+        addUserStatement.close();
     }
 
     public void resetDatabase() throws SQLException {
-        Statement rdata = connection.createStatement();
-        rdata.executeUpdate("drop database if exists schuldenapp;");
-        rdata.executeUpdate("create database schuldenapp;");
-        rdata.executeUpdate("use schuldenapp;");
-        rdata.executeUpdate("create table nutzer(\n" +
+        Statement resetDatabaseStatement = connection.createStatement();
+        resetDatabaseStatement.executeUpdate("drop database if exists schuldenapp;");
+        resetDatabaseStatement.executeUpdate("create database schuldenapp;");
+        resetDatabaseStatement.executeUpdate("use schuldenapp;");
+        resetDatabaseStatement.executeUpdate("create table nutzer(\n" +
                 "IDnutzer int Not Null auto_increment,\n" +
                 "nutzername varchar(10) not null default '',\n" +
                 "passwort varchar(20) not null default '',\n" +
                 "primary key (IDnutzer));");
-        rdata.executeUpdate("create table einträge(\n" +
+        resetDatabaseStatement.executeUpdate("create table einträge(\n" +
                 "IDeintrag int(2) NOT NULL auto_increment,\n" +
                 "datum datetime,\n" +
                 "betrag decimal NOT NULL,\n" +
@@ -153,37 +145,37 @@ public class FamilyFinanceManagement {
                 "foreign key (vorstreckerID) REFERENCES nutzer(IDnutzer),\n" +
                 "foreign key (bezahlerID) REFERENCES nutzer(IDnutzer)\n" +
                 ");");
-        rdata.close();
+        resetDatabaseStatement.close();
     }
 
-    public void eintragHinzufügen() throws SQLException {
-        String datum = "1111-11-11";
-        double betrag = 0;
-        double stand = 0;
-        String kommentar = "Keine Nutzereingabe";
-        int vorstrID = 0;
-        int bezID = 0;
+    public void addEntry() throws SQLException {
+        String date = "1111-11-11";
+        double amount;
+        double balance = 0;
+        String comment;
+        int lenderID;
+        int debtorID;
 
         System.out.println("Welche Datumsoption? (1) Heute  (2) Eigenes Datum?");
         switch (sc.nextInt()) {
             case 1:
-                datum = String.valueOf(LocalDate.now());
+                date = String.valueOf(LocalDate.now());
                 break;
             case 2:
                 System.out.println("Datum eingeben (YYYY-MM-DD)");
-                datum = sc.next();
+                date = sc.next();
                 break;
         }
         System.out.println("Betrag:");
-        betrag = sc.nextDouble();
+        amount = sc.nextDouble();
         System.out.println("Kommentar (MAX 255 CHARACTERS)");
-        kommentar = sc.next();
-        nutzerausgeben();
+        comment = sc.next();
+        outputAllUser();
         System.out.println("Vorstreckender: (ID)");
-        vorstrID = sc.nextInt();
+        lenderID = sc.nextInt();
         System.out.println("Bezahlender: (ID)");
-        bezID = sc.nextInt();
-        PreparedStatement ps = connection.prepareStatement("insert into einträge values (null, '" + datum + "' , " + betrag + ", " + stand + ", '" + kommentar + "', " + vorstrID + ", " + bezID+ ");");
+        debtorID = sc.nextInt();
+        PreparedStatement ps = connection.prepareStatement("insert into einträge values (null, '" + date + "' , " + amount + ", " + balance + ", '" + comment + "', " + lenderID + ", " + debtorID + ");");
         ps.execute();
     }
 }
